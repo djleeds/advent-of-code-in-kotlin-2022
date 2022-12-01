813e0d237d9531b3f675f304d108069120fd0a38
Dave Leeds <dave@typealias.com>
2022-12-01 12:10:01 -0600

    Add solution for Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,29 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    data class Food(val calories: Int)
+    data class Elf(val inventory: List<Food>) {
+        val calorieCount = inventory.sumOf { it.calories }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun <T> List<T>.split(boundary: (T) -> Boolean): List<List<T>> {
+        val original = this
+        return buildList {
+            var lastBoundary = 0
+            original.forEachIndexed { index: Int, value: T ->
+                if (boundary(value)) {
+                    add(original.subList(lastBoundary, index))
+                    lastBoundary = index + 1
+                }
+            }
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun elves(input: List<String>) = input.split { it.isEmpty() }.map { data -> Elf(data.map { it.toInt() }.map(::Food)) }
+
+    fun part1(input: List<String>): Int =
+        elves(input).maxBy { it.calorieCount }.calorieCount
+
+    fun part2(input: List<String>): Int =
+        elves(input).sortedByDescending { it.calorieCount }.take(3).sumOf { it.calorieCount }
 
     val input = readInput("Day01")
     println(part1(input))
