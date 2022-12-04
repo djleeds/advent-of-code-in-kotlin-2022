ace77ff183948ec4526df169e1ecf2ee65bca1e1
Dave Leeds <dave@typealias.com>
2022-12-04 14:29:06 -0600

    Refactor Day 04 to clarify types and operations

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,24 +1,32 @@
+private class Assignment(start: Int, end: Int) {
+    val range = start..end
+
+    infix fun containsAllOf(other: Assignment) = range.intersect(other.range).size == other.range.count()
+    infix fun containsAnyOf(other: Assignment) = range.intersect(other.range).isNotEmpty()
+
+    companion object {
+        fun parse(input: String) = input.split("-").map { id -> id.toInt() }.let { (start, end) -> Assignment(start, end) }
+    }
+}
+
+private class AssignmentPair(val first: Assignment, val second: Assignment) {
+    fun hasFullOverlap() = first containsAllOf second || second containsAllOf first
+    fun hasAnyOverlap() = first containsAnyOf second || second containsAnyOf first
+
+    companion object {
+        fun parse(input: String) = input.split(",").map(Assignment::parse).let { (first, second) -> AssignmentPair(first, second) }
+    }
+}
+
 fun main() {
-    operator fun IntRange.contains(other: IntRange) = intersect(other).size == other.count()
-    fun Pair<IntRange, IntRange>.hasFullOverlap() = first in second || second in first
+    fun part1(input: List<AssignmentPair>): Int = input.count { it.hasFullOverlap() }
+    fun part2(input: List<AssignmentPair>): Int = input.count { it.hasAnyOverlap() }
 
-    infix fun IntRange.containsAnyOf(other: IntRange) = intersect(other).isNotEmpty()
-    fun Pair<IntRange, IntRange>.hasAnyOverlap() = first containsAnyOf second || second containsAnyOf first
-
-    fun parseLine(line: String): Pair<IntRange, IntRange> =
-        line
-            .split(",")
-            .map { it.split("-").map { id -> id.toInt() }.let { (start, end) -> IntRange(start, end) } }
-            .let { (assignment1, assignment2) -> assignment1 to assignment2 }
-
-    fun part1(input: List<String>): Int = input.map(::parseLine).count { it.hasFullOverlap() }
-    fun part2(input: List<String>): Int = input.map(::parseLine).count { it.hasAnyOverlap() }
-
-    val testInput = readInput("Day04_test")
+    val testInput = readInput("Day04_test").map(AssignmentPair::parse)
     check(part1(testInput) == 2)
     check(part2(testInput) == 4)
 
-    val input = readInput("Day04")
+    val input = readInput("Day04").map(AssignmentPair::parse)
     println(part1(input))
     println(part2(input))
 }
