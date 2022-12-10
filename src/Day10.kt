5064424ad2bd1a7e79441a55b8ac9e70fb0a6863
Dave Leeds <dave@typealias.com>
2022-12-10 12:21:23 -0600

    Checkpoint on refactor

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,39 +1,45 @@
+sealed class Instruction(val cycles: Int, val operation: (previousX: Int) -> Int) {
+    class NoOp : Instruction(1, { it })
+    class AddX(private val amount: Int) : Instruction(2, { it + amount })
+    companion object {
+        fun parse(line: String) = when {
+            line == "noop"          -> NoOp()
+            line.startsWith("addx") -> AddX(line.substringAfter(" ").toInt())
+            else                    -> throw IllegalArgumentException()
+        }
+    }
+}
+
+class CPU {
+    val xHistory: MutableList<Int> = mutableListOf(1)
+    private val x: Int get() = xHistory.last()
+    private fun tick(value: Int = x) = xHistory.add(value)
+
+    fun process(instruction: Instruction) {
+        repeat(instruction.cycles - 1) { tick() }
+        tick(instruction.operation(x))
+    }
+}
+
+
 fun main() {
     fun part1(instructions: List<String>): Int {
-        val registerValues = mutableListOf(1) // represents the value AFTER the tick of the index
-        instructions.forEach { instruction ->
-            when {
-                instruction == "noop"          -> registerValues.add(registerValues.last())
-                instruction.startsWith("addx") -> {
-                    val amount = instruction.substringAfter(" ").toInt()
-                    registerValues.add(registerValues.last())
-                    registerValues.add(registerValues.last().plus(amount))
-                }
-            }
-        }
+        val cpu = CPU()
+        instructions.map(Instruction::parse).forEach(cpu::process)
 
         return listOf(20, 60, 100, 140, 180, 220)
             // registerValues represents AFTER the tick. Applying -1 here to get DURING the tick.
-            .onEach { println("Cycle $it - X is ${registerValues[it - 1]}") }
-            .sumOf { registerValues[it - 1] * it }
+            .onEach { println("Cycle $it - X is ${cpu.xHistory[it - 1]}") }
+            .sumOf { cpu.xHistory[it - 1] * it }
     }
 
     fun part2(instructions: List<String>) {
-        val registerValues = mutableListOf(1) // represents the value AFTER the tick of the index
-        instructions.forEach { instruction ->
-            when {
-                instruction == "noop"          -> registerValues.add(registerValues.last())
-                instruction.startsWith("addx") -> {
-                    val amount = instruction.substringAfter(" ").toInt()
-                    registerValues.add(registerValues.last())
-                    registerValues.add(registerValues.last().plus(amount))
-                }
-            }
-        }
+        val cpu = CPU()
+        instructions.map(Instruction::parse).forEach(cpu::process)
 
         val pixels = mutableListOf<Boolean>()
 
-        registerValues.chunked(40).forEachIndexed { startingAt, values ->
+        cpu.xHistory.chunked(40).forEachIndexed { startingAt, values ->
             values.forEachIndexed { x, horizontalPosition ->
                 val cycle = startingAt + horizontalPosition
                 pixels.add(x in (cycle - 1 - startingAt)..(cycle + 1 - startingAt))
