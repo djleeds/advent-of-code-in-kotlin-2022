8e5f312154b42b983b97f8b91960e2186d28e19d
Dave Leeds <dave@typealias.com>
2022-12-10 19:13:19 -0600

    Part 2 is broken still

--- src/Day10.kt
+++ src/Day10.kt
@@ -19,26 +19,22 @@ sealed class Instruction(val cycles: Int, val operation: (previousX: Int) -> Int
 class CPU {
     val signals = Channel<Pair<Int, Int>>()
 
-    val history: MutableList<Int> = mutableListOf(0, 1)
-    private val x: Int get() = history.lastOrNull() ?: 1
-    private fun tick(value: Int = x) = history.add(value)
+    private var x: Int = 1
+    private var cycle: Int = 1
 
-    fun execute(program: List<Instruction>) = program.forEach { instruction ->
-        repeat(instruction.cycles - 1) { tick() }
-        tick(instruction.operation(x))
+    private suspend fun tick(value: Int = x) {
+        cycle++
+        x = value
+        signals.send(cycle to value)
     }
 
-    suspend fun execute2(program: List<Instruction>) {
-        println("A")
-        signals.send(history.lastIndex to x)
-        println("B")
+    suspend fun execute(program: List<Instruction>) {
+        signals.send(cycle to x)
         program.forEach { instruction ->
-            repeat(instruction.cycles - 1) { tick(); signals.send(history.lastIndex to x) }
-            tick(instruction.operation(x)); signals.send(history.lastIndex to x)
+            repeat(instruction.cycles - 1) { tick(); }
+            tick(instruction.operation(x))
         }
-        println("C")
         signals.close()
-        println("D")
     }
 }
 
@@ -48,71 +44,37 @@ data class Sprite(private val width: Int, private val position: Int) {
     fun movedTo(position: Int) = Sprite(width, position)
 }
 
-class CRT(private val width: Int, height: Int) {
-    private val pixels: MutableList<Boolean> = MutableList((width * height) + 1) { false }
-
-    fun tick(cycle: Int, sprite: Sprite) {
-        val renderingAt = cycle.rem(width)
-        pixels[cycle] = renderingAt in sprite.occupiedPixels
-    }
-
-    fun render(on: String = "#", off: String = ".") =
-        pixels.joinToString("") { if (it) on else off }.chunked(40).forEach(::println)
-}
-
-class CRT2(private val width: Int, height: Int, private val signals: Channel<Pair<Int, Int>>) {
+class CRT(private val width: Int, height: Int, private val signals: Channel<Pair<Int, Int>>) {
     private var sprite: Sprite = Sprite(3, 0)
-    private val pixels: MutableList<Boolean> = MutableList((width * height) + 100) { false }
-
-    fun tick(cycle: Int, sprite: Sprite) {
-        val renderingAt = cycle.rem(width)
-        pixels[cycle] = renderingAt in sprite.occupiedPixels
-    }
+    private val pixels: MutableList<Boolean> = MutableList((width * height) + 2) { false }
 
     suspend fun listen() = signals.consumeEach { (cycle, x) ->
-        println("$cycle = $x")
         sprite = sprite.movedTo(x)
         pixels[cycle] = cycle.rem(width) in sprite.occupiedPixels
     }
 
     fun render(on: String = "#", off: String = ".") {
-        pixels.joinToString("") { if (it) on else off }.chunked(40).forEach(::println)
+        pixels.joinToString("") { if (it) on else off }.windowed(40, 40).forEach(::println)
     }
 }
 
 
 fun main() = runBlocking {
-    fun part1(instructions: List<String>): Int {
-        val cpu = CPU().apply { execute(instructions.map(Instruction::parse)) }
-        return listOf(20, 60, 100, 140, 180, 220).sumOf { cpu.history[it] * it }
-    }
-
-    suspend fun part1b(instructions: List<String>): Int {
+    suspend fun part1(instructions: List<String>): Int {
         val cpu = CPU()
 
-        launch { cpu.execute2(instructions.map(Instruction::parse)) }
+        launch { cpu.execute(instructions.map(Instruction::parse)) }
 
         return cpu.signals.toList()
             .filter { (cycle, _) -> cycle in listOf(20, 60, 100, 140, 180, 220) }
             .sumOf { (cycle, x) -> cycle * x }
     }
 
-    fun part2(instructions: List<String>) {
-        val crt = CRT(40, 6)
-
-        CPU()
-            .apply { execute(instructions.map(Instruction::parse)) }
-            .history
-            .forEachIndexed { cycle, x -> crt.tick(cycle, Sprite(3, x)) }
-
-        crt.render()
-    }
-
-    suspend fun part2b(instructions: List<String>) {
+    suspend fun part2(instructions: List<String>) {
         val cpu = CPU()
-        val crt = CRT2(40, 6, cpu.signals)
+        val crt = CRT(40, 6, cpu.signals)
 
-        launch { cpu.execute2(instructions.map(Instruction::parse)) }
+        launch { cpu.execute(instructions.map(Instruction::parse)) }
 
         crt.listen()
         crt.render()
@@ -121,11 +83,9 @@ fun main() = runBlocking {
 
     val testInput = readInput("Day10_test")
     println(part1(testInput))
-    println(part1b(testInput))
-    println(part2(testInput))
-    println(part2b(testInput))
-//
-//    val input = readInput("Day10")
-//    println(part1(input))
-//    println(part2(input))
+    part2(testInput)
+
+    val input = readInput("Day10")
+    println(part1(input))
+    part2(input)
 }
